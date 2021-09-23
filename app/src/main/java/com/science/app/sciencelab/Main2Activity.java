b00ce5d7c7105b9782482e9ae25c1d17bfd81e68
Hamza PC <shamihamza25@gmail.com>
2021-09-23 22:13:20 +0300

    commit

--- app/src/main/java/com/science/app/sciencelab/Main2Activity.java
+++ app/src/main/java/com/science/app/sciencelab/Main2Activity.java
@@ -25,20 +25,19 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
         initViews();
 
         List<Item> deviceName = new ArrayList<>();
-        Item item1 = new Item(1,"الأجهزة الميكانيكية","",1,R.drawable.ic_mec);
-        Item item2 = new Item(2,"الأجهزة الكهربائية","",2,R.drawable.ic_electr);
-        Item item3 = new Item(3,"كهرباء ساكنة","",3,R.drawable.ic_static);
-        Item item4 = new Item(4,"أجهزة مغناطيسية","",4,R.drawable.ic_magnetic);
-        Item item5 = new Item(5,"أجهزة حرارة","",5,R.drawable.temp_1);
-        Item item6 = new Item(6,"أجهزة ضوئية","",6,R.drawable.ic_light);
-        Item item7 = new Item(7,"أجهزة كيميائية","",7,R.drawable.ic_chem);
-        Item item8 = new Item(8,"أجهزة أحياء","",8,R.drawable.ic_biology);
-        Item item9 = new Item(9,"زجاجيات","",9,R.drawable.ic_glass);
-        Item item10 = new Item(10,"أجهزة مساعدة","",10,R.drawable.ic_helper);
+        Item item1 = new Item(1, "الأجهزة الميكانيكية", "", 1, R.drawable.ic_mec);
+        Item item2 = new Item(2, "الأجهزة الكهربائية", "", 2, R.drawable.ic_electr);
+        Item item3 = new Item(3, "كهرباء ساكنة", "", 3, R.drawable.ic_static);
+        Item item4 = new Item(4, "أجهزة مغناطيسية", "", 4, R.drawable.ic_magnetic);
+        Item item5 = new Item(5, "أجهزة حرارة", "", 5, R.drawable.temp_1);
+        Item item6 = new Item(6, "أجهزة ضوئية", "", 6, R.drawable.ic_light);
+        Item item7 = new Item(7, "أجهزة كيميائية", "", 7, R.drawable.ic_chem);
+        Item item8 = new Item(8, "أجهزة أحياء", "", 8, R.drawable.ic_biology);
+        Item item9 = new Item(9, "زجاجيات", "", 9, R.drawable.ic_glass);
+        Item item10 = new Item(10, "أجهزة مساعدة", "", 10, R.drawable.ic_helper);
 
         deviceName.add(item1);
         deviceName.add(item2);
@@ -51,33 +50,28 @@ public class Main2Activity extends AppCompatActivity {
         deviceName.add(item9);
         deviceName.add(item10);
 
-
-
-
         RecyclerView recyclerView = findViewById(R.id.since_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecyclerViewAdapter(this, deviceName);
-
-
         recyclerView.setAdapter(adapter);
     }
 
     private void initViews() {
 
-        recyclerView=findViewById(R.id.since_rv);
+        recyclerView = findViewById(R.id.since_rv);
         toolbar = findViewById(R.id.toolbar_main);
-        title_toolbar=findViewById(R.id.title_toolbar);
+        title_toolbar = findViewById(R.id.title_toolbar);
         title_toolbar.setText("Lab Guide");
         title_toolbar.setTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
 
+    }
 
-    }
     public void onBackPressed() {
-            super.onBackPressed();
-            finish();
-            finishAffinity();
-        }
+        super.onBackPressed();
+        finish();
+        finishAffinity();
     }
+}
 
